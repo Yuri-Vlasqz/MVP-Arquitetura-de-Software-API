16be3b66eb5b9530ba9ffd602e6fc48b427fb874
Yuri-Vlasqz <yurivlasqz@gmail.com>
2025-04-13 14:31:18 -0300

    fix: swagger doc

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from routes.usuarios import usuarios_bp
 # - 1.5.0 -> Adaptacão para MVP de Arquitetura de Software.
 # - 1.6.0 -> Implementação de APIBlueprint e mudança de estrutura do projeto.
 # - 1.7.0 -> Implementação de autenticação com Auth0 para rotas protegidas.
-info = Info(title="Teste API - Todos a ver", version="1.7.0")
+info = Info(title="Teste API - Tudo a ver", version="2.0.0")
 app = OpenAPI(__name__, info=info, security_schemes=security_schemes)
 CORS(app)  # Desenvolvimento local
 
@@ -29,7 +29,7 @@ def home():
 
 
 app.register_api(tmdb_bp)            # Chamadas ao TMDB para obtenção de informações de programas
-app.register_api(usuarios_bp)        # Adição, visualização, atualização e remoção de usuários
+app.register_api(usuarios_bp)        # Adição e visualização de usuários
 app.register_api(listas_bp)          # Adição, atualização e remoção de listas
 app.register_api(programa_lista_bp)  # Adição e remoção de programas de uma lista
 
